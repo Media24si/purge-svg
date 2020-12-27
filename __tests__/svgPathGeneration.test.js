74801ca56eb8d0f6502b0220c964f284456c4561
Jernej Pogacar <jernej.pogacar@flycom.si>
2020-12-27 12:34:59 +0100

    update npm dependencies to latest and fix eslint errors

--- __tests__/svgPathGeneration.test.js
+++ __tests__/svgPathGeneration.test.js
@@ -38,30 +38,30 @@ describe('svg paths generation', () => {
 
     it('should keep same name but set folder for output', () => {
         expect(PurgeSvg.prepareSvgPaths([
-            { in: `${root}clean_svgs/icons.svg`, 'out': '/foo/bar' },
+            { in: `${root}clean_svgs/icons.svg`, out: '/foo/bar' },
         ])).toEqual([
             {
                 filename: 'icons.svg',
                 in: `${rootPath}/__tests__/test_examples/clean_svgs/icons.svg`,
-                out: `/foo/bar/icons.svg`,
+                out: '/foo/bar/icons.svg',
                 prefix: '',
             },
         ])
 
         expect(PurgeSvg.prepareSvgPaths([
-            { in: `${root}clean_svgs/icons.svg`, 'out': '/foo/bar' },
-            { in: `${root}clean_svgs/icons-2.svg`, 'out': '/foo/bar' },
+            { in: `${root}clean_svgs/icons.svg`, out: '/foo/bar' },
+            { in: `${root}clean_svgs/icons-2.svg`, out: '/foo/bar' },
         ])).toEqual([
             {
                 filename: 'icons.svg',
                 in: `${rootPath}/__tests__/test_examples/clean_svgs/icons.svg`,
-                out: `/foo/bar/icons.svg`,
+                out: '/foo/bar/icons.svg',
                 prefix: '',
             },
             {
                 filename: 'icons-2.svg',
                 in: `${rootPath}/__tests__/test_examples/clean_svgs/icons-2.svg`,
-                out: `/foo/bar/icons-2.svg`,
+                out: '/foo/bar/icons-2.svg',
                 prefix: '',
             },
         ])
